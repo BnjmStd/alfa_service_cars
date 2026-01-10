19c812f509797742f44ab4e46fff7ea715d70cfa
bnjmn <benja_e_e@hotmail.com>
2026-01-09 22:58:42 -0300

    mejoras seo

--- src/lib/social.ts
+++ src/lib/social.ts
@@ -20,20 +20,19 @@ export const SOCIALS: SocialLink[] = [
     platform: "instagram",
     url: "https://www.instagram.com/alfa.detailers?igsh=MW11ODlvMTlocmxkZw%3D%3D&utm_source=qr",
   },
-  // {
-  //   label: "Facebook",
-  //   platform: "facebook",
-  //   url: "https://facebook.com/alfadetailersstudio",
-  // },
-  // {
-  //   label: "TikTok",
-  //   platform: "tiktok",
-  //   url: "https://tiktok.com/@alfadetailersstudio",
-  // },
+  {
+    label: "Facebook",
+    platform: "facebook",
+    url: "https://www.facebook.com/share/17stJ4MLWF/?mibextid=wwXIfr",
+  },
+  {
+    label: "TikTok",
+    platform: "tiktok",
+    url: "https://www.tiktok.com/@alfadetailers?_r=1&_t=ZM-92wMcKOOMSi",
+  },
   {
     label: "WhatsApp",
     platform: "whatsapp",
-    // Usar el número exportado desde getPhoneNumber.ts
     url: `https://wa.me/${CLIENT_PHONE_NUMBER.replace(/\D/g, "")}`,
   },
 ];
